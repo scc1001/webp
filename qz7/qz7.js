5fbbec36c68a7ced0db3cad04193a8ecc8e8a7e4
scc1001 <100269107+scc1001@users.noreply.github.com>
2022-04-27 16:58:16 +0800

    Create qz7.js

---
+++ qz7/qz7.js
@@ -0,0 +1,41 @@
+{
+    "count": 6,
+    "data": [
+        {
+            "stationNo": "C0AD1",
+            "stationName": "八里",
+            "recTime": "202204271600",
+            "rain": 0
+        },
+        {
+            "stationNo": "C0AD0",
+            "stationName": "三芝",
+            "recTime": "202204271600",
+            "rain": 0
+        },
+        {
+            "stationNo": "C0AD4",
+            "stationName": "土城",
+            "recTime": "202204271600",
+            "rain": 0
+        },
+        {
+            "stationNo": "318",
+            "stationName": "大安福州山",
+            "recTime": "202204271602",
+            "rain": 0
+        },
+        {
+            "stationNo": "01A17",
+            "stationName": "坪林",
+            "recTime": "201911130140",
+            "rain": 0
+        },
+        {
+            "stationNo": "C0AD2",
+            "stationName": "深坑",
+            "recTime": "",
+            "rain": 0
+        }
+    ]
+}
