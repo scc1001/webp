43daeabbac390755131c8e1c8cd40269b5e23cec
scc1001 <100269107+scc1001@users.noreply.github.com>
2022-05-18 15:10:08 +0800

    Add files via upload

---
+++ qz8/src/cgu_hello.js
@@ -0,0 +1,11 @@
+function cguhello() {
+    return (
+      <div className="cguhello">
+        <header className="cguhello-header">
+          <h1>hello cgu</h1>
+        </header>
+      </div>
+    );
+  }
+  
+  export default cguhello;
